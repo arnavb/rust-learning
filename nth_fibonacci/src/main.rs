04a0142588faeb516c8b26acf47740f2ca21f131
Arnav Borborah <arnavborborah11@gmail.com>
2018-10-18 19:51:48 +0000

    Add code for nth fibonacci number

---
+++ nth_fibonacci/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{self, Write};
+
+fn main() {
+    let input: u64 = loop {
+        print!("Enter a positive integer: ");
+        io::stdout().flush().expect("Unable to flush STDOUT!");
+
+        let mut result = String::new();
+    
+        io::stdin().read_line(&mut result).expect("Unable to read input!");
+        
+        match result.trim().parse() {
+            Ok(number) => break number,
+            Err(_) => {
+                println!("The value you entered was not a valid positive integer!");
+                continue;
+            }
+        }   
+    };
+    
+    println!("Fibonacci #{} = {}", input, nth_fibonacci(input));
+}
+
+fn nth_fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+    
+    let mut previous2;
+    let mut previous1 = 0;
+    let mut current = 1;
+    
+    for _i in 0..(n - 1) {
+        previous2 = previous1;
+        previous1 = current;
+        current = previous1 + previous2;
+    }
+    current
+}
